1689163d2772dbc11e4187133c9fc7f49de64148
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-14 21:49:26 +0200

    Added SceneAtmosphereEditor.cs

---
+++ Assets/Editor/SceneAtmosphereEditor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SceneAtmosphereEditor : EditorWindow
+{
+    LightingSettings lightingSettingsProfile;
+
+    //
+    bool infoFO = false;
+    bool sceneSettingsFO = false;
+    bool lightProfileSettingsFO = false;
+
+    [MenuItem("Tools/Atmosphere")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(SceneAtmosphereEditor));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Atmosphere editor", EditorStyles.boldLabel);
+
+        infoFO = EditorGUILayout.Foldout(infoFO, "Info");
+        if (infoFO){ ShowInfo(); }
+
+        sceneSettingsFO = EditorGUILayout.Foldout(sceneSettingsFO, "Scene Settings");
+        if (sceneSettingsFO) { ShowSceneSettings(); }
+
+        lightProfileSettingsFO = EditorGUILayout.Foldout(lightProfileSettingsFO, "Light Profile Settings");
+        if (lightProfileSettingsFO) { ShowLightProfileSettings(); }
+    }
+
+    void ShowInfo()
+    {
+        GUILayout.BeginVertical("Box");
+        GUILayout.Label("Info", EditorStyles.boldLabel);
+
+        GUILayout.EndVertical();
+    }
+
+    void ShowSceneSettings()
+    {
+        GUILayout.BeginVertical("Box");
+        GUILayout.Label("Scene Settings", EditorStyles.boldLabel);
+        GUILayout.EndVertical();
+    }
+
+    void ShowLightProfileSettings()
+    {
+        GUILayout.BeginVertical("Box");
+        GUILayout.Label("Light Profile Settings", EditorStyles.boldLabel);
+        GUILayout.EndVertical();
+    }
+}
