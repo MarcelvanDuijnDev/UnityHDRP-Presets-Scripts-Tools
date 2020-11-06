0c3e55d92f9e4e3606a22ca745faba8f2ea3b768
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-11-06 20:33:15 +0100

    Update ExampleTestScript.cs

--- Assets/Scenes/ExampleTestScript.cs
+++ Assets/Scenes/ExampleTestScript.cs
@@ -11,7 +11,7 @@ public class ExampleTestScript : MonoBehaviour
 
     [Header("ObjectPool")] [SerializeField] private ObjectPool _ObjectPool;
     [Header("LoadSave_JSON")] [SerializeField] private SaveLoad_JSON _SaveLoadJSON;
-    [SerializeField] private SaveData _SaveDataExample = new SaveData();
+    [SerializeField] private Json_SaveData _SaveDataExample = new Json_SaveData();
     [Header("Showinfo")]
     [SerializeField] private TextMeshProUGUI _InfoText;
     [SerializeField] private TextMeshProUGUI _InfoText2;
@@ -46,7 +46,7 @@ public class ExampleTestScript : MonoBehaviour
                     _InfoText2.text = "Savedata Loaded " + System.DateTime.Now + "\n";
                     for (int i = 0; i < _SaveDataExample.saveData.Count; i++)
                     {
-                        _InfoText2.text += _SaveDataExample.saveData[i].randomfloat.ToString() + " \n";
+                        _InfoText2.text += _SaveDataExample.saveData[i].exampleValue.ToString() + " \n";
                     }
                 }
                 if(Input.GetKeyDown(KeyCode.N))
