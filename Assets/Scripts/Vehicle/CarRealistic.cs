5bb890c1f993fa24ae2df4c90036e423356f7d1e
Marcel van Duijn <epicmars264@gmail.com>
2020-12-30 13:41:30 +0100

    Added CarRealistic.cs

---
+++ Assets/Scripts/Vehicle/CarRealistic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRealistic : MonoBehaviour
+{
+    [SerializeField] private List<AxleInfo> axleInfos = null;
+    [SerializeField] private float maxMotorTorque = 1000;
+    [SerializeField] private float maxSteeringAngle = 50;
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
