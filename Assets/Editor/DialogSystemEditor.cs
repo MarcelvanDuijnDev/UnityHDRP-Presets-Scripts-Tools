fb2e9a3ccaf65f97aecc81b0b4784fbb0a0573be
Marcel van Duijn <epicmars264@gmail.com>
2021-12-21 17:29:25 +0100

    Changes:
    Changes:
    
    * Added AudioZone.cs
    * Added AudioZoneEditor.cs
    * Added DialogSystemEditor.cs
    
    * Updated Tool_QuickStart,cs
    * Updated Tool_ScriptToString.cs
    * Updated AudioHandler.cs
    * Updated Interactable.cs
    * Updated InteractionHandler.cs
    * Updated UIEffects.cs
    
    * Removed AudioZoneSphere.cs
    * Removed AudioZoneBox.cs

---
+++ Assets/Editor/DialogSystemEditor.cs
@@ -0,0 +1,238 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+class DialogSystemEditor : EditorWindow
+{
+    DialogSystem _Dialog;
+    string _DialogLoad;
+
+    Vector2 _ScrollPos_TimeLine = new Vector2();
+    Vector2 _ScrollPos_Editor = new Vector2();
+
+    Vector2Int _Selected = new Vector2Int();
+
+    bool _SearchScript = false;
+
+    [MenuItem("Tools/DialogSystem Editor")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(DialogSystemEditor));
+    }
+
+    void OnGUI()
+    {
+        if (!_SearchScript)
+            _Dialog = GameObject.FindObjectOfType<DialogSystem>();
+
+        GUILayout.Label("Dialog Editor", EditorStyles.boldLabel);
+        _Dialog = EditorGUILayout.ObjectField(_Dialog, typeof(DialogSystem), true) as DialogSystem;
+
+        if (_Dialog == null)
+            return;
+        _Dialog._Dialog.DialogNameID = EditorGUILayout.TextField("DialogName",_Dialog._Dialog.DialogNameID);
+
+        EditorGUILayout.BeginHorizontal("box");
+        if (_Dialog != null)
+        {
+            //Editor
+            Editor();
+
+            //TimeLine
+            TimeLine();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void Editor()
+    {
+        EditorGUILayout.BeginVertical("box");
+
+        //Edit
+        _ScrollPos_Editor = EditorGUILayout.BeginScrollView(_ScrollPos_Editor, GUILayout.Width(325));
+        if (_Selected.x >= 0)
+        {
+            if (_Selected.x < _Dialog._Dialog.DialogTree.Count)
+            {
+                if (_Selected.y < _Dialog._Dialog.DialogTree[_Selected.x].Dialog.Count)
+                {
+                    //Dialog
+                    GUILayout.Label("Selected   " + "ID:" + _Selected.x.ToString() + "," + _Selected.y.ToString());
+                    _Dialog._Dialog.DialogTree[_Selected.x].DialogTreeInfo = EditorGUILayout.TextField("Row Info:", _Dialog._Dialog.DialogTree[_Selected.x].DialogTreeInfo);
+                    _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Dialog = EditorGUILayout.TextArea(_Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Dialog, GUILayout.Height(200), GUILayout.Width(300));
+
+                    //Dialog Options
+                    GUILayout.Label("Options");
+                    EditorGUILayout.BeginVertical("box");
+
+                    int optionscount = 0;
+                    for (int i = 0; i < _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options.Count; i++)
+                    {
+                        optionscount++;
+                        //Toggle
+                        _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].OptionToggle = EditorGUILayout.Foldout(_Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].OptionToggle, "(" + optionscount.ToString() + ") " + _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].OptionInfo);
+                        
+                        //Options
+                        if (_Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].OptionToggle)
+                        {
+                            //Option Dialog
+                            GUILayout.Label("Option Dialog:");
+                            _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].OptionInfo = EditorGUILayout.TextArea(_Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].OptionInfo, GUILayout.Height(100), GUILayout.Width(300));
+
+                            //Display options
+                            EditorGUILayout.BeginVertical("box");
+                            for (int o = 0; o < _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options.Count; o++)
+                            {
+                                //Option dropdown/Remove Event
+                                EditorGUILayout.BeginHorizontal();
+                                _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options[o].Option = (DialogSystem_DialogOption.Options)EditorGUILayout.EnumPopup(_Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options[o].Option);
+                                if (GUILayout.Button("-", GUILayout.Width(20)))
+                                {
+                                    _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options.RemoveAt(o);
+                                    break;
+                                }
+                                EditorGUILayout.EndHorizontal();
+
+                                //Options
+                                switch (_Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options[o].Option)
+                                {
+                                    case DialogSystem_DialogOption.Options.GOTO:
+                                        _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options[o].NextID = EditorGUILayout.Vector2IntField("Next ID", _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options[o].NextID);
+                                        break;
+                                }
+
+                            }
+                            if (GUILayout.Button("Add Event"))
+                            {
+                                DialogSystem_DialogOption newoption = new DialogSystem_DialogOption();
+                                _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options[i].Options.Add(newoption);
+                            }
+                            EditorGUILayout.EndVertical();
+                        }
+                    }
+                    if(GUILayout.Button("Add Option Dialog"))
+                    {
+                        DialogSystem_DialogOptions newoption = new DialogSystem_DialogOptions();
+                        newoption.OptionInfo = "new option";
+                        _Dialog._Dialog.DialogTree[_Selected.x].Dialog[_Selected.y].Options.Add(newoption);
+                    }
+                    EditorGUILayout.EndVertical();
+                }
+                else
+                {
+                    GUILayout.Label("Selected");
+                    GUILayout.Label("ID: --");
+                    GUILayout.Label("Press a button on \nthe timeline to select!");
+                }
+            }
+        }
+        else
+        {
+            GUILayout.Label("Selected");
+            GUILayout.Label("ID: --");
+            GUILayout.Label("Press a button on \nthe timeline to select!");
+        }
+
+        //SaveLoad
+        if(GUILayout.Button("Save"))
+        {
+            SaveData();
+        }
+
+        _DialogLoad = EditorGUILayout.TextField("Dialog Path:", _DialogLoad);
+        if (GUILayout.Button("Load"))
+        {
+            LoadData();
+        }
+
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+    }
+
+    void TimeLine()
+    {
+        EditorGUILayout.BeginVertical();
+        _ScrollPos_TimeLine = EditorGUILayout.BeginScrollView(_ScrollPos_TimeLine);
+        for (int i = 0; i < _Dialog._Dialog.DialogTree.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal("box");
+
+            // Row Options
+            EditorGUILayout.BeginVertical();
+
+            // ID/Remove
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("-", GUILayout.Width(20)))
+            {
+                _Dialog._Dialog.DialogTree.RemoveAt(i);
+                if(_Selected.x > _Dialog._Dialog.DialogTree.Count-1)
+                    _Selected.x--;
+                break;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            // Add Dialog to timeline
+            if (GUILayout.Button("+", GUILayout.Width(20)))
+            {
+                DialogSystem_Dialog newdialog = new DialogSystem_Dialog();
+                newdialog.Dialog = "dialogtext";
+                _Dialog._Dialog.DialogTree[i].Dialog.Add(newdialog);
+            }
+
+            EditorGUILayout.EndVertical();
+
+            //TimeLineButtons
+            for (int j = 0; j < 100; j++)
+            {
+                EditorGUILayout.BeginVertical();
+                GUILayout.Label(j.ToString());
+
+                if (j < _Dialog._Dialog.DialogTree[i].Dialog.Count)
+                {
+                    //if (GUILayout.Button("(" + _Dialog.Dialog[i].DialogTree[j].Options.Count.ToString() + ") " + _Dialog.Dialog[i].DialogTree[j].Dialog, GUILayout.Width(100), GUILayout.Height(30)))
+                    if (GUILayout.Button(j.ToString() + "    (" + _Dialog._Dialog.DialogTree[i].Dialog[j].Options.Count.ToString() + ") ", GUILayout.Width(100), GUILayout.Height(30)))
+                    {
+                        _Selected = new Vector2Int(i, j);
+                    }
+                }
+
+                EditorGUILayout.EndVertical();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        // Add row
+        if (GUILayout.Button("Add Dialog Tree", GUILayout.Width(100), GUILayout.Height(50)))
+        {
+            DialogSystem_DialogTree newdialogtree = new DialogSystem_DialogTree();
+            DialogSystem_Dialog newdialog = new DialogSystem_Dialog();
+            newdialog.Dialog = "dialogtext";
+            newdialogtree.Dialog.Add(newdialog);
+            _Dialog._Dialog.DialogTree.Add(newdialogtree);
+        }
+
+        EditorGUILayout.EndScrollView();
+        EditorGUILayout.EndVertical();
+    }
+
+    //SaveLoad
+    public void SaveData()
+    {
+        string jsonData = JsonUtility.ToJson(_Dialog._Dialog, true);
+        File.WriteAllText(Application.dataPath + "/" + _Dialog._Dialog.DialogNameID + ".json", jsonData);
+    }
+    public void LoadData()
+    {
+        try
+        {
+            string dataAsJson = File.ReadAllText(Application.dataPath + "/" + _DialogLoad + ".json");
+            _Dialog._Dialog = JsonUtility.FromJson<DialogSystem_File>(dataAsJson);
+        }
+        catch
+        {
+            SaveData();
+        }
+    }
+}
--- Assets/Scripts/Audio/AudioHandler.cs
+++ Assets/Scripts/Audio/AudioHandler.cs
@@ -29,7 +29,29 @@ public class AudioHandler : MonoBehaviour
             //AudioSource
             if (_Sound[i].Settings.CreateAudioSource)
             {
-                _Sound[i].Settings.AudioSource = this.gameObject.AddComponent<AudioSource>();
+                //3D Space
+                if (_Sound[i].Audio3D.Enable3DAudio)
+                {
+                    //Create new object
+                    GameObject audiopos = new GameObject("Audio_" + _Sound[i].AudioTrackName);
+
+                    //Set audiopos position
+                    if (_Sound[i].Audio3D.SpatialTransform != null)
+                        audiopos.transform.position = _Sound[i].Audio3D.SpatialTransform.position;
+                    else
+                        audiopos.transform.position = _Sound[i].Audio3D.SpatialPosition;
+                    audiopos.transform.parent = this.gameObject.transform;
+
+                    //Add AudioSource to audioposition
+                    _Sound[i].Settings.AudioSource = audiopos.AddComponent<AudioSource>();
+                }
+                else
+                    _Sound[i].Settings.AudioSource = this.gameObject.AddComponent<AudioSource>();
+
+                //SetVolume
+                _Sound[i].Settings.AudioSource.volume = _Sound[i].AudioSettings.Volume;
+
+                //AudioMixer
                 _Sound[i].Settings.AudioSource.outputAudioMixerGroup = _AudioMixer;
 
                 //AudioGroup
@@ -37,23 +59,32 @@ public class AudioHandler : MonoBehaviour
                     _Sound[i].Settings.AudioSource.outputAudioMixerGroup = _Sound[i].Settings.AudioGroup;
             }
 
+            //3D Space Settings
+            if (_Sound[i].Audio3D.Enable3DAudio)
+            {
+                _Sound[i].Settings.AudioSource.spatialBlend = 1;
+            }
+
             //AudioClip
             _Sound[i].Settings.AudioSource.clip = _Sound[i].Settings.AudioClip;
 
             //Settings
-            if (_Sound[i].AudioSettings.PlayOnStart)
+            if (!_Sound[i].AudioSettings.PlayOnStart_DiplicateOnly)
             {
-                _Sound[i].Settings.AudioSource.playOnAwake = _Sound[i].AudioSettings.PlayOnStart;
-                _Sound[i].Settings.AudioSource.Play();
-            }
-            if (_Sound[i].AudioEffects.FadeIn)
-            {
-                _Sound[i].Settings.AudioSource.volume = 1;
-                _Sound[i].AudioEffects.FadeInSpeed = _Sound[i].AudioSettings.Volume / _Sound[i].AudioEffects.FadeInDuration;
-            }
-            if (_Sound[i].AudioEffects.FadeOut)
-            {
-                _Sound[i].AudioEffects.FadeOutSpeed = _Sound[i].AudioSettings.Volume / _Sound[i].AudioEffects.FadeOutDuration;
+                if (_Sound[i].AudioSettings.PlayOnStart)
+                {
+                    _Sound[i].Settings.AudioSource.playOnAwake = _Sound[i].AudioSettings.PlayOnStart;
+                    _Sound[i].Settings.AudioSource.Play();
+                }
+                if (_Sound[i].AudioEffects.FadeIn)
+                {
+                    _Sound[i].Settings.AudioSource.volume = 1;
+                    _Sound[i].AudioEffects.FadeInSpeed = _Sound[i].AudioSettings.Volume / _Sound[i].AudioEffects.FadeInDuration;
+                }
+                if (_Sound[i].AudioEffects.FadeOut)
+                {
+                    _Sound[i].AudioEffects.FadeOutSpeed = _Sound[i].AudioSettings.Volume / _Sound[i].AudioEffects.FadeOutDuration;
+                }
             }
         }
 
@@ -69,12 +100,6 @@ public class AudioHandler : MonoBehaviour
 
         for (int i = 0; i < _Sound.Count; i++)
         {
-            if(!_Sound[i].AudioEffects.FadingIn && !_Sound[i].AudioEffects.FadingOut)
-            {
-                _Sound[i].Settings.AudioSource.volume = _Sound[i].AudioSettings.Volume;
-                continue;
-            }
-
             //FadeIn
             if (_Sound[i].AudioEffects.FadingIn)
             {
@@ -271,7 +296,7 @@ public class AudioHandler : MonoBehaviour
             if (_Sound[i].AudioTrackName == trackname)
                 return i;
         }
-        return 0;
+        return -1;
     }
 
     /// <summary>Refresh settings.</summary>
@@ -289,7 +314,99 @@ public class AudioHandler : MonoBehaviour
         }
     }
 
+    /// <summary>Duplicate AudioTrack.</summary>
+    public string DuplicateAudioTrack(string trackname)
+    {
+        int audioid = Get_Track_ID(trackname);
+        if (audioid == -1)
+            return null;
 
+        AudioHandler_Sound newsound = new AudioHandler_Sound();
+        GameObject newaudiopos = new GameObject();
+
+        newsound.AudioTrackName = "Audio_" + _Sound[audioid].AudioTrackName;
+
+        //Settings
+        newsound.Settings = new AudioHandler_Settings();
+        newsound.Settings.AudioClip = _Sound[audioid].Settings.AudioClip;
+        newsound.Settings.AudioGroup = _Sound[audioid].Settings.AudioGroup;
+        newsound.Settings.AudioSource = newaudiopos.AddComponent<AudioSource>();
+        newsound.Settings.CreateAudioSource = _Sound[audioid].Settings.CreateAudioSource;
+
+        //Control
+        newsound.AudioControl = new AudioHandler_Control();
+        newsound.AudioControl.SceneEnabled = _Sound[audioid].AudioControl.SceneEnabled;
+        newsound.AudioControl.StartAudioOnScene = _Sound[audioid].AudioControl.StartAudioOnScene;
+        newsound.AudioControl.StopAudioOnScene = _Sound[audioid].AudioControl.StopAudioOnScene;
+        newsound.AudioControl.StopOnNextScene = _Sound[audioid].AudioControl.StopOnNextScene;
+
+        //Audio3D
+        newsound.Audio3D = new AudioHandler_3DAudio();
+        newsound.Audio3D.Enable3DAudio = _Sound[audioid].Audio3D.Enable3DAudio;
+        newsound.Audio3D.SpatialPosition = _Sound[audioid].Audio3D.SpatialPosition;
+        newsound.Audio3D.SpatialTransform = _Sound[audioid].Audio3D.SpatialTransform;
+
+        //AudioSettings
+        newsound.AudioSettings = new AudioHandler_AudioSettings();
+        newsound.AudioSettings.Loop = _Sound[audioid].AudioSettings.Loop;
+        newsound.AudioSettings.MaxVolume = _Sound[audioid].AudioSettings.MaxVolume;
+        newsound.AudioSettings.PlayOnStart = _Sound[audioid].AudioSettings.PlayOnStart;
+        newsound.AudioSettings.Volume = _Sound[audioid].AudioSettings.Volume;
+
+        //AudioEffect
+        newsound.AudioEffects = new AudioHandler_Effects();
+        newsound.AudioEffects.FadeIn = _Sound[audioid].AudioEffects.FadeIn;
+        newsound.AudioEffects.FadeInDone = _Sound[audioid].AudioEffects.FadeInDone;
+        newsound.AudioEffects.FadeInDuration = _Sound[audioid].AudioEffects.FadeInDuration;
+        newsound.AudioEffects.FadeInSpeed = _Sound[audioid].AudioEffects.FadeInSpeed;
+        newsound.AudioEffects.FadeOut = _Sound[audioid].AudioEffects.FadeOut;
+        newsound.AudioEffects.FadeOutAfterTime = _Sound[audioid].AudioEffects.FadeOutAfterTime;
+        newsound.AudioEffects.FadeOutDone = _Sound[audioid].AudioEffects.FadeOutDone;
+        newsound.AudioEffects.FadeOutDuration = _Sound[audioid].AudioEffects.FadeOutDuration;
+        newsound.AudioEffects.FadeOutSpeed = _Sound[audioid].AudioEffects.FadeOutSpeed;
+        newsound.AudioEffects.FadingIn = _Sound[audioid].AudioEffects.FadingIn;
+        newsound.AudioEffects.FadingOut = _Sound[audioid].AudioEffects.FadingOut;
+
+        newsound.AudioTrackName += "_" + _Sound.Count.ToString();
+
+        //Activate Settings
+        newsound.Settings.AudioSource.loop = newsound.AudioSettings.Loop;
+        newsound.Settings.AudioSource.volume = newsound.AudioSettings.Volume;
+        if (newsound.Audio3D.Enable3DAudio)
+            newsound.Settings.AudioSource.spatialBlend = 1;
+        if (newsound.AudioSettings.PlayOnStart)
+            newsound.Settings.AudioSource.Play();
+
+        //Create new object
+        newaudiopos.transform.name = newsound.AudioTrackName;
+
+        //Audio Source Settings
+        newsound.Settings.AudioSource.clip = newsound.Settings.AudioClip;
+        newsound.Settings.AudioSource.outputAudioMixerGroup = newsound.Settings.AudioGroup;
+
+        //Position
+        if (newsound.Audio3D.SpatialTransform != null)
+            ChangeAudioPosition(newsound.AudioTrackName, newsound.Audio3D.SpatialTransform.position);
+        else
+            ChangeAudioPosition(newsound.AudioTrackName, newsound.Audio3D.SpatialPosition);
+
+        //PlayOnStart
+        if (newsound.AudioSettings.PlayOnStart)
+            newsound.Settings.AudioSource.Play();
+
+        //Apply
+        newaudiopos.transform.parent = this.transform;
+        _Sound.Add(newsound);
+        return newsound.AudioTrackName;
+    }
+
+    /// <summary>Change AudioSource Position.</summary>
+    public void ChangeAudioPosition(string trackname, Vector3 newpos)
+    {
+        int audioid = Get_Track_ID(trackname);
+        if (audioid != -1)
+            _Sound[audioid].Settings.AudioSource.transform.position = newpos;
+    }
 }
 
 [System.Serializable]
@@ -298,6 +415,7 @@ public class AudioHandler_Sound
     public string AudioTrackName;
     public AudioHandler_Settings Settings;
     public AudioHandler_AudioSettings AudioSettings;
+    public AudioHandler_3DAudio Audio3D;
     public AudioHandler_Control AudioControl;
     public AudioHandler_Effects AudioEffects;
 }
@@ -318,10 +436,11 @@ public class AudioHandler_Settings
 public class AudioHandler_AudioSettings
 {
     [Header("AudioSettings")]
-    [Range(0, 1)] public float Volume;
-    [Range(0, 1)] public float MaxVolume;
+    [Range(0, 1)] public float Volume = 1;
+    [Range(0, 1)] public float MaxVolume = 1;
     public bool Loop;
     public bool PlayOnStart;
+    public bool PlayOnStart_DiplicateOnly;
 }
 
 [System.Serializable]
@@ -350,4 +469,13 @@ public class AudioHandler_Effects
     [HideInInspector] public float FadeOutSpeed;
     [HideInInspector] public bool FadeOutDone;
     [HideInInspector] public bool FadingOut;
+}
+
+[System.Serializable]
+public class AudioHandler_3DAudio
+{
+    [Header("3D Space / (0,0,0)+null = this object position")]
+    public bool Enable3DAudio;
+    public Vector3 SpatialPosition;
+    public Transform SpatialTransform;
 }
